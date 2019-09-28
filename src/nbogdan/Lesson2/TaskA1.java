ac0f4f1803d7709cc1714ac5d6e8ce94667912ab
Woof <7883494@gmail.com>
2019-09-28 15:00:02 +0300

    task A1

--- src/nbogdan/Lesson2/TaskA1.java
+++ src/nbogdan/Lesson2/TaskA1.java
@@ -1,7 +1,17 @@
 package nbogdan.Lesson2;
 
+import java.util.Scanner;
+
 public class TaskA1 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        double m, n, a;
+        System.out.println("Введите 2 числа");
+        m = sc.nextDouble();
+        n = sc.nextDouble();
+        if (Math.abs(10 -m) != Math.abs(10 - n)) {
+            a = Math.abs(10 - m) > Math.abs(10 - n) ? n : m;
+            System.out.println(a);
+        } else {System.out.println("Числа равноудалены от 10");}
     }
 }
