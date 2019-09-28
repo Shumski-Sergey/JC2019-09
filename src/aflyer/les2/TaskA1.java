f6de9c7d40b9a47dbaa459ad06803a4ec6d1d94a
Flyer-jr <Flyer-jr@yandex.com>
2019-09-28 14:40:39 +0300

    the beginning

--- src/aflyer/les2/TaskA1.java
+++ src/aflyer/les2/TaskA1.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class TaskA1 {
         public static void main(String[] args) {
-            Scanner
+            Scanner sc=new Scanner(System.in);
+            System.out.println("ВВедите числа");
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            int c = (10-m)>(10-n)?  n : m;
+            System.out.println(c);
         }
 }
