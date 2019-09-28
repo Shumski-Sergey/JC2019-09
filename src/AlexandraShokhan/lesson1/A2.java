74e0c20caf659cc943c3801b5166667c533f058a
Alexandra <alexandra.shokhan@gmail.com>
2019-09-28 19:36:50 +0300

    A new folder was created. Task 2 updated. Task 3 done.

--- src/AlexandraShokhan/lesson1/A2.java
+++ src/AlexandraShokhan/lesson1/A2.java
@@ -5,12 +5,12 @@ public class A2 {
     double a = 1;
     double b = -8;
     double c = 15;
-    double D = Math.pow(b, 2) - 4 * a * c;
-    if (D < 0) {
+    double d = Math.pow(b, 2) - 4 * a * c;
+    if (d < 0) {
         System.out.println("Equation has no roots");
     } else {
-        double root1 = (-b + Math.sqrt(D)) / (2 * a);
-        double root2 = (-b - Math.sqrt(D)) / (2 * a);
+        double root1 = (-b + Math.sqrt(d)) / (2 * a);
+        double root2 = (-b - Math.sqrt(d)) / (2 * a);
         System.out.println("Root 1 = " + root1);
         System.out.println("Root 2 = " + root2);
     }
---
+++ src/AlexandraShokhan/lesson1/A3.java
@@ -0,0 +1,30 @@
+package AlexandraShokhan.lesson1;
+
+public class A3 {
+    public static void main(String[] args) {
+    int a = 15;
+    int b = 49;
+    int c = 8;
+    int v; // This variable is used to temporally store other values.
+
+    if (a < b) {
+        v = a;
+        a = b;
+        b = v;
+    }
+
+    if (b < c) {
+        v = b;
+        b = c;
+        c = v;
+    }
+
+    if (a < b) {
+        v = a;
+        a = b;
+        b = v;
+    }
+
+    System.out.println(a + " " + b + " " + c);
+    }
+}
