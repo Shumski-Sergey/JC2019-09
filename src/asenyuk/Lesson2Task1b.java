d4efb1e6fed36f9be0415acd64e6a48f63029557
asenyuk <avsenyuk@hotmail.com>
2019-09-30 22:56:37 +0300

    lesson2task1b

---
+++ src/asenyuk/Lesson2Task1b.java
@@ -0,0 +1,19 @@
+package asenyuk;
+
+public class Lesson2Task1b {
+    public static void main(String[] args) {
+        int max=155;
+        int min=5;
+
+        double a = (Math.random() * (max-min +1)) +min;
+        System.out.println("Number = " +a);
+
+        if (25<a & a<100) {
+            System.out.println( "Good number");
+
+        }
+        else {
+            System.out.println( "Bad number");
+        }
+    }
+}
