84adeb1f0eed53577812e163f0824ec0a08d4bf6
Burkut60 <burkut60@gmail.com>
2019-09-30 22:57:48 +0300

    1

--- src/balexej/TaskA3.java
+++ src/balexej/TaskA3.java
@@ -1,16 +1,31 @@
 package balexej;
-import java.util.Scanner;
 
 
+      public class TaskA3 {
 
-public class TaskA3 {
-    public static void main(String[] args){
-        Scanner q = new Scanner(System.in);
-        System.out.println("Введите 3 любых целых числа");
-        int a = q.nextInt();
-        int b = q.nextInt();
-        int c = q.nextInt();
-        System.out.println(a + b + c );
+    public static void main(String[] args) {
+
+        int a =7;
+        int b = 9;
+        int c = 5;
+
+
+        if (a < b & b < c)
+    System.out.println(a + " " + b + " " + c);
+
+        if (c < a & a < b)
+            System.out.println(c+ " " + a + " " + b);
+
+if ( b < a & a < c)
+    System.out.println(b + " " + a + " " + c);
+
+        if (b < c & c < a)
+
+            System.out.println(b + " " + c + " " + a);
+
+        if (b < a & c < b)
+
+            System.out.println(c + " " + b + " " + a);
 
     }
 }
