45c5fe92cec292a8c5bcc1eba476b1d23dcaada1
Burkut60 <burkut60@gmail.com>
2019-10-01 00:51:14 +0300

    1

--- src/balexej/TaskA3.java
+++ src/balexej/TaskA3.java
@@ -8,17 +8,19 @@ package balexej;
         int a =1;
         int b = 7;
         int c = 5;
-     if (a < c & c < b)
-         System.out.println(a + " " + c + " " + b);
+
+
+        if (a < c & c < b)
+          System.out.println(a + " " + c + " " + b);
 
         if (a < b & b < c)
-    System.out.println(a + " " + b + " " + c);
+          System.out.println(a + " " + b + " " + c);
 
         if (c < a & a < b)
             System.out.println(c+ " " + a + " " + b);
 
-if ( b < a & a < c)
-    System.out.println(b + " " + a + " " + c);
+        if ( b < a & a < c)
+            System.out.println(b + " " + a + " " + c);
 
         if (b < c & c < a)
 
