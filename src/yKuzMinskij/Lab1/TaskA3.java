6f1e6ac77d86505d10190d3c1c4b16ecd6826077
Yury6720 <kuzminskij5683415@gmail.com>
2019-10-01 23:22:43 +0300

    Sorting numbers

--- src/yKuzMinskij/Lab1/TaskA3.java
+++ src/yKuzMinskij/Lab1/TaskA3.java
@@ -4,21 +4,26 @@ import java.util.Scanner;
 
 public class TaskA3 {
     public static void main (String[] args) {
-        System.out.println("Введите три числа");
+        System.out.println("Введите три целых неравных между собой числа числа");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        for ((a < b) && (b < c)) {
-            int a1 = a;
-            a = b;
-            b = a1;
-            a1 = c;
-            c = b;
-            b = a1;
-            System.out.println(a + " " + b + " " + c);
-        }
-
-        //if ((a!=b)&&(a!= c)&&(b!= c)) {}
+        int v = 0;
+        while ((a > b) || (b > c) || (a > c)) {
+            if (b > c) {
+                v = b;
+                b = c;
+                c = v;
+            } else if (a > b) {
+                v = a;
+                a = b;
+                b = v;
+            } else if (a > c){
+                v=a;
+                a=c;
+                c=v;
+            }
+        } System.out.println(a + " " + b + " " + c);
     }
 }
