4725e6892a460a1caf6f62b15af85c9126655877
Burkut60 <burkut60@gmail.com>
2019-10-01 20:58:29 +0300

    1

--- src/balexej/TaskB2.java
+++ src/balexej/TaskB2.java
@@ -2,7 +2,8 @@ package balexej;
 
 public class TaskB2 {
     public static void main(String[] args ){
-        int i = 6;
+        int i = 5;
+        System.out.println(i);
         while (i>1){
             i = i-1;
             System.out.println(i);
