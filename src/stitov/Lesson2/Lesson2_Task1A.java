35d13eb97220e38c7c4704a017aa9f5fec6fcc0d
MiroshnichenkoA <55538046+MiroshnichenkoA@users.noreply.github.com>
2019-10-02 23:58:54 +0300

    Update HW Lesson2

--- src/stitov/Lesson2/Lesson2_Task1A.java
+++ src/stitov/Lesson2/Lesson2_Task1A.java
@@ -1,20 +1,24 @@
 package stitov.Lesson2;
 
+import java.util.Scanner;
 /**
  * Created by user on 28.09.2019.
  */
 public class Lesson2_Task1A {
   public static void main(String[] args) {
     double m, n, a = 10;
-    m = -9;
-    n = 30;
+    System.out.println("Введите первое число");
+    Scanner in = new Scanner(System.in);
+    m = in.nextDouble();
+    System.out.println("Введите второе число");
+    n = in.nextDouble();
     if (Math.abs(m - a) == Math.abs(n - a)) {
       System.out.print("Расстояние одинаково");
     } else {
       if (Math.abs(m - a) < Math.abs(n - a)) {
-        System.out.print("Ближайшее " + m);
+        System.out.print("Ближайшее к 10: " + m);
       } else {
-        System.out.print("Ближайшее " + n);
+        System.out.print("Ближайшее к 10: " + n);
       }
     }
   }
--- src/stitov/Lesson2/Lesson2_Task2A.java
+++ src/stitov/Lesson2/Lesson2_Task2A.java
@@ -1,14 +1,38 @@
 package stitov.Lesson2;
-
+import java.util.Scanner;
 /**
  * Created by user on 28.09.2019.
  */
 public class Lesson2_Task2A {
   public static void main(String[] args) {
-    double a, b, c, d, x1, x2;
-    a = 1;
-    b = 1;
-    c = -12;
+    double a, b, c;
+    Scanner in = new Scanner(System.in);
+    System.out.println("Введите a");
+    a = in.nextDouble();
+    System.out.println("Введите b");
+    b = in.nextDouble();
+    System.out.println("Введите c");
+    c = in.nextDouble();
+    if (a == 0 && b != 0) {
+      oneroot(b, 2);
+    } else if (a == 0 && b == 0) {
+      noroots();
+    } else {
+      haveroots(a, b, c);
+    }
+  }
+
+  public static void oneroot(double b, double c) {
+    double x = -1 * c / b;
+    System.out.println("Корень равен " + x);
+  }
+
+  public static void noroots() {
+    System.out.println("Отсутствуют неизвестные");
+  }
+
+  public static void haveroots(double a, double b, double c) {
+    double d, x1, x2;
     d = b * b - 4 * a * c;
     if (d < 0) {
       System.out.print("Вещественных корней нет");
