620e9cf00bdb1ba43a1504ace8effdf2d6ee4df0
Flyer-jr <Flyer-jr@yandex.com>
2019-10-02 14:00:41 +0300

    changed Task B3

--- src/aflyer/les2/TaskB3.java
+++ src/aflyer/les2/TaskB3.java
@@ -9,7 +9,7 @@ public class TaskB3 {
         int a = sc.nextInt();
         int sum=0;
         for (int i=1; i<=a; i++)
-            sum=sum+i;
+            sum += i;
         System.out.println(sum);
     }
 }
