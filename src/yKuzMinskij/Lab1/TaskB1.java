0ec9726e21f205359690f6506d521750837d2140
Yury6720 <kuzminskij5683415@gmail.com>
2019-10-02 03:27:35 +0300

    Sorting numbers

--- src/yKuzMinskij/Lab1/TaskB1.java
+++ src/yKuzMinskij/Lab1/TaskB1.java
@@ -3,7 +3,7 @@ package yKuzMinskij.Lab1;
 public class TaskB1 {
     public static void main (String[] args) {
         int i = 5 + (int) (Math.random() * 155);
-        if (i>25 && i<100) {
+        if (i>=25 && i<=100) {
             System.out.println("Случайное число " + i +" находится в диапазоне 25-100");
         }else System.out.println("Случайное число " + i + " не находится в диапазоне 25-100");
     }
