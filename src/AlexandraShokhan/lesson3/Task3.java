478486e442fd0bb1c373b7235694976147aa8517
Alexandra <alexandra.shokhan@gmail.com>
2019-10-04 21:06:33 +0300

    Lesson 3: Task 3 fixed.

--- src/AlexandraShokhan/lesson3/Task3.java
+++ src/AlexandraShokhan/lesson3/Task3.java
@@ -4,54 +4,23 @@ package AlexandraShokhan.lesson3;
 3. Имеется целое число, следует вывести его в бухгалтерском формате.
 Т.е. начиная справа каждые три позиции отделяются пробелом. Например число
 20023143 должно быть выведено как 20 023 143
-
+*/
 
 public class Task3 {
     public static void main(String[] args) {
-        int inputNumber = 56745678;
-        int number = inputNumber;
-
+        int inputNumber = 5746000;
+        String str1 = Integer.toString(inputNumber);
+        int length1 = str1.length();
+        int numberOfSpaces = (int) Math.floor(length1 / 3);
         int counter = 0;
 
-        while (number > 0) {
+        StringBuilder str = new StringBuilder(Integer.toString(inputNumber));
+
+        for (int i = str.length() - 3; counter < numberOfSpaces; i-=3) {
+            str.insert(i, " ");
             counter++;
-            number = number / 1000;
-        }
-        int[] array;
-        array = new int[counter];
-
-        for (int i = array.length - 1; i >= 0; i--) {
-            array[i] = inputNumber % 1000;
-            inputNumber = inputNumber / 1000;
         }
 
-        for (int j = array.length; j > 0; j--) {
-            System.out.print(" " + array[array.length - j]);
-        }
-        }
+        System.out.println(str);
     }
-*/
-public class Task3 {
-    public static void main(String[] args) {
-        int inputNumber = 5674560;
-        int number = inputNumber;
-        String str = Integer.toString(inputNumber);
-
-
-        for (int i = 0; i <= str.length() - 1; i++) {
-            System.out.print(str.charAt(i));
-            if ((str.length() % 3 == 1) && (i == 0)) {
-                System.out.print(" ");
-            }
-            System.out.print(str.charAt(i + 1));
-            if ((str.length() % 3 == 2) && (i == 0)) {
-                System.out.print(" ");
-            }
-                System.out.print(str.charAt(i + 2));
-                if (str.length() % 3 == 0) {
-                    System.out.print(" ");
-                    i += 2;
-                }
-            }
-        }
-    }
+}
