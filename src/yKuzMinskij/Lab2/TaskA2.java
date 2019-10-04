1829df625dc0337b0e94fc62cc0dd29c30b29913
Yury6720 <kuzminskij5683415@gmail.com>
2019-10-05 01:47:06 +0300

    TaskA2 Checking prime number

--- src/yKuzMinskij/Lab2/TaskA2.java
+++ src/yKuzMinskij/Lab2/TaskA2.java
@@ -7,10 +7,11 @@ public class TaskA2 {
         System.out.println("Введите число");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        for (int i = 2; i<num; i++){
-            if (num%i==0) {
-                System.out.println("составное");break;
-            }else if (i==num-1)System.out.println("простое");
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                System.out.println("составное");
+                break;
+            } else if (i == num - 1) System.out.println("простое");
         }
     }
 }
