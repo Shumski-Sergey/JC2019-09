3d70bd33098a4375d3e9c953f788c75103f41372
Yury6720 <kuzminskij5683415@gmail.com>
2019-10-05 01:51:46 +0300

    TaskB1 Natural number factorial

--- src/yKuzMinskij/Lab2/TaskB1.java
+++ src/yKuzMinskij/Lab2/TaskB1.java
@@ -7,8 +7,8 @@ public class TaskB1 {
         System.out.println("Введите число");
         Scanner sc = new Scanner(System.in);
         long num = sc.nextLong();
-        int f= 1;
-        for (int i = 1; i<=num; i++){
+        int f = 1;
+        for (int i = 1; i <= num; i++) {
             f *= i;
         }
         System.out.println(f);
