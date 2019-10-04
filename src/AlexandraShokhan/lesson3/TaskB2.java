9d2fff7c2d12414a5d2cd41e318d91219fde56b5
Alexandra <alexandra.shokhan@gmail.com>
2019-10-04 19:44:37 +0300

    Lesson 3: Task B2 done.

--- src/AlexandraShokhan/lesson3/TaskB2.java
+++ src/AlexandraShokhan/lesson3/TaskB2.java
@@ -2,5 +2,22 @@ package AlexandraShokhan.lesson3;
 
 // 2. Выведите на экран все положительные делители натурального числа, введённого пользователем с клавиатуры.
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class TaskB2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a positive integer number: ");
+        int num = in.nextInt();
+
+        ArrayList dividers = new ArrayList<>();
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                dividers.add(i);
+            }
+        }
+        System.out.println(dividers);
+    }
 }
