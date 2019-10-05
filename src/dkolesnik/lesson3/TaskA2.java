0dc264d2683a0a268bf704a7b4bb668edab50deb
dkolesnik <dashaband1996@gmail.com>
2019-10-05 14:33:05 +0300

    add taska1, a2, b1

---
+++ src/dkolesnik/lesson3/TaskA2.java
@@ -0,0 +1,26 @@
+package dkolesnik.lesson3;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+        int k;
+        int i;
+        boolean t = true;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите целое число:");
+        k = in.nextInt();
+        for (i = 2; i < k; i++) {
+            if (k % i == 0) {
+                t = false;
+                break}
+            }
+        } if (f)
+
+
+    }
+
+}
---
+++ src/dkolesnik/lesson3/TaskB1.java
@@ -0,0 +1,7 @@
+package dkolesnik.lesson3;
+
+public class TaskB1 {
+    public static void main (String[] args) {
+
+    }
+}
