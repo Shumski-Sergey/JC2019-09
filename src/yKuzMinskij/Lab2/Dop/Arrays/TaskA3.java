9334f232374631d54aac1d19a98c008b6d63328b
Yury6720 <kuzminskij5683415@gmail.com>
2019-10-05 04:38:28 +0300

    AddTaskA3 Sequence 90, 85, 80, 75, 70...

--- src/yKuzMinskij/Lab2/Dop/Arrays/TaskA3.java
+++ src/yKuzMinskij/Lab2/Dop/Arrays/TaskA3.java
@@ -2,7 +2,7 @@ package yKuzMinskij.Lab2.Dop.Arrays;
 
 public class TaskA3 {
     public static void main (String[] args) {
-        int num = 150;
+        int num = 90;
         while (num>=0) {
             for (int i=0; i<10&&num>=0; i++){
                 System.out.print(num + " ");
