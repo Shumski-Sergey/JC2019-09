bc5a42c2cb0a5e9352d70923b9e75f0a6effb6e5
mikulionk <dimaborovka@gmail.com>
2019-10-06 03:06:38 +0300

    lesson4

---
+++ src/dmikulionak/lesson_4/task1_massive.java
@@ -0,0 +1,27 @@
+package dmikulionak.lesson_4;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class task1_massive {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+        }
+        System.out.println(Arrays.toString(array));             //вывод массива
+        int max;
+        max = array[0];
+        for (int value : array) {                               //what?
+            if (value > max) {
+                max = value;
+            }
+        }
+        for (int i=0; i< array.length; i++) {
+            if (array[i] == max) {
+                System.out.print(i);
+            }
+        }
+    }
+}
