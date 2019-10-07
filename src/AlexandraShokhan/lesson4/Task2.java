4b688e853feecf3f5c02288e5d8208fbd636c611
Alexandra <alexandra.shokhan@gmail.com>
2019-10-07 19:28:19 +0300

    Lesson 4: Tasks 2 drafted.

--- src/AlexandraShokhan/lesson4/Task2.java
+++ src/AlexandraShokhan/lesson4/Task2.java
@@ -1,9 +1,31 @@
 package AlexandraShokhan.lesson4;
 
-//
+// 1.В задаче на поиск максимальной оценки, вывести не саму оценку, а ее номер.
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Task2 {
     public static void main(String[] args) {
+        int [] grades;
+        grades = new int[30];
 
+        for (int i = 0; i <= grades.length; i++) {
+            grades[i] = getRandomNum(1,10);
+            System.out.println( i + " " + grades[i]);
+        }
+
+
+
+
+
+        int a = getRandomNum(1, 10);
+        System.out.println(a);
     }
+    public static int getRandomNum(int min, int max) {
+        int i = ThreadLocalRandom.current().nextInt(min, max + 1);
+        return i;
+        }
+
+
 }
+
