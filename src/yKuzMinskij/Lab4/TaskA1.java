18628bc0d2284393280ba57797644e32842d10ee
Yury6720 <kuzminskij5683415@gmail.com>
2019-10-07 19:20:31 +0300

    TaskA1 Creating marks array. Outputting this marks array max mark position.

---
+++ src/yKuzMinskij/Lab4/TaskA1.java
@@ -0,0 +1,33 @@
+package yKuzMinskij.Lab4;
+
+public class TaskA1 {
+    public static void main (String[] args) {
+      positionOfMaxMarks();
+      }
+      public static int[] creatingMarksArray () {
+          int [] marksArray = new int[100];
+          for (int i=0; i!=marksArray.length; i++) {
+              marksArray[i] = (int) (Math.random() * 10);
+          }
+          return marksArray;
+      }
+      public static int maxMark (int [] marksArray){
+        int maxMark=0;
+        for (int i=0; i<marksArray.length; i++ ){
+            if (marksArray[i]>maxMark){
+                maxMark = marksArray [i];
+            }
+        }
+        return maxMark;
+    }
+      public static void positionOfMaxMarks (){
+        int [] marksArray = creatingMarksArray();
+        int maxMark = maxMark(marksArray);
+        for (int i=0; i<marksArray.length; i++){
+            if (marksArray[i]==maxMark){
+                System.out.println("Максимальная оценка " + maxMark+ " находится на позиции " + i);
+            }
+        }
+
+      }
+}
