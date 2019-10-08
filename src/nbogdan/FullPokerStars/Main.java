154b01eee693b2962ab1891bcca12a50357dc391
Woof <7883494@gmail.com>
2019-10-08 22:01:12 +0300

    1.1.0 Beta Poker update. Run Main.java, say "y/yes/da/да to play"

--- src/nbogdan/FullPokerStars/Main.java
+++ src/nbogdan/FullPokerStars/Main.java
@@ -2,6 +2,9 @@ package nbogdan.FullPokerStars;
 
 import java.util.Scanner;
 /* created by PizzaDog 05.06.2019
+* update 1.1.0 Beta 08.10.2019:
+* Добавлена проверка комбинаций кроме Royal Flush, Straight Flush, Straight
+* Игра разделена на раздачи
 *
 * update 1.0.1 pre-alpha:
 * Компьютер раздает карты из 1 колоды максимум на 10 игроков (по классике Texas Holdem)
