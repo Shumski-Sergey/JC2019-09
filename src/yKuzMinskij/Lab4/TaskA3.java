7103c189ffe400afb081a866f94ecc6958f125ac
Yury6720 <kuzminskij5683415@gmail.com>
2019-10-09 05:50:45 +0300

    TaskA3

---
+++ src/yKuzMinskij/Lab4/TaskA3.java
@@ -0,0 +1,36 @@
+package yKuzMinskij.Lab4;
+//Создать массив, заполнить его случайными элементами, распечатать,
+//перевернуть, и снова распечатать (при переворачивании нежелательно создавать
+//еще один массив).
+
+public class TaskA3 {
+    public static void main (String[] args) {
+        int[] numsArray = creatingNumsArray();
+        outputArray1(numsArray);
+        System.out.println("\n\n");
+        outputArray2(numsArray);
+    }
+    public static int[] creatingNumsArray (){
+        int[] numArray=new int[100];
+        for (int i=0; i<numArray.length; i++){
+            numArray [i]= (int) (Math.random()*100);
+        }
+        return numArray;
+    }
+    public static void outputArray1 (int[] numsArray){
+        for (int i=0; i<numsArray.length;){
+            for(int k=0; k<10; k++){
+                System.out.print(numsArray[i] + " ");
+                i++;
+            }System.out.println();
+        }
+    }
+    public static void outputArray2 (int[] numsArray){
+        for (int i = (numsArray.length - 1); i>0;){
+            for (int k=0; k<10; k++){
+                System.out.print(numsArray[i] + " ");
+                --i;
+            }System.out.println();
+        }
+    }
+}
