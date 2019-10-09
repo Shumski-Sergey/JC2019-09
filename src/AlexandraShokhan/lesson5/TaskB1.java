ee2c2e9805ab0e9ff291abffa9510626c6364201
Alexandra <alexandra.shokhan@gmail.com>
2019-10-09 23:03:01 +0300

    Lesson 5

---
+++ src/AlexandraShokhan/lesson5/TaskB1.java
@@ -0,0 +1,12 @@
+package AlexandraShokhan.lesson5;
+
+//1.Напишите два цикла выполняющих многократное сложение строк, один
+//с помощью оператора сложения и String, а другой с помощью StringBuilder
+//и метода append. Сравните скорость их выполнения.
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        String string [];
+        string = new String[10] {}
+    }
+}
