d4b2876afe28c3f65315734d0ab56b8c38e8eff2
Alexandra <alexandra.shokhan@gmail.com>
2019-10-09 11:15:53 +0300

    Lesson 4.

--- src/AlexandraShokhan/lesson4/Task6.java
+++ src/AlexandraShokhan/lesson4/Task6.java
@@ -3,5 +3,16 @@ package AlexandraShokhan.lesson4;
 // 1.Найти в строке не только запятые, но и другие знаки препинания. Подсчитать
 //общее их количество
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task6 {
+    public static void main(String[] args) {
+        String text = "Quick six blind smart, out burst. Perfectly on furniture dejection: determine my depending; an to. Add short, water - court fat.";
+        int numOfPunctuationMarks = 0;
+        while(text.matches("[$&+,:;=?@#|'<>.-^*()%!]")) {
+    numOfPunctuationMarks++;
+            System.out.print("The number of punctuation marks in the text is " + numOfPunctuationMarks);
+        }
+    }
 }
