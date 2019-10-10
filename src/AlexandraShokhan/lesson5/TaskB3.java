d33363ba327680534ee690b8424e0a4f1e8193f8
Alexandra <alexandra.shokhan@gmail.com>
2019-10-10 23:12:22 +0300

    Lesson 5

---
+++ src/AlexandraShokhan/lesson5/TaskB3.java
@@ -0,0 +1,10 @@
+package AlexandraShokhan.lesson5;
+
+//3. Доменные имена для протоколов http и https, с необязательным слешем в конце. Специальые символы не используются.
+//Примеры:
+//http://example.com/ — Да
+//example.com — Нет
+//кремль.рф — Нет :(
+
+public class TaskB3 {
+}
