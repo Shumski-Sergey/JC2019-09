3f9722a15993209a30d7c4f2859d5c458d21cb51
Alexandra <alexandra.shokhan@gmail.com>
2019-10-13 19:03:31 +0300

    Lesson 5: Task 6 drafted.

--- src/AlexandraShokhan/lesson6/Time.java
+++ src/AlexandraShokhan/lesson6/Time.java
@@ -7,31 +7,52 @@ package AlexandraShokhan.lesson6;
 //два конструктора: получающий общее количество секунд, и часы, минуты и секунды
 //по отдельности. Сделать метод для вывода данных. Прочее на ваше усмотрение.
 
+
 public class Time {
     public static void main(String[] args) {
-        TimeInterval day = new TimeInterval();
-        day.hours = 24;
-        int secInDay = day.getTimeInSec();
 
-        TimeInterval hour = new TimeInterval();
-        hour.minutes = 60;
-        int secInHour = hour.getTimeInSec();
+        // Создаем два объекта класса TimeInterval
+        TimeInterval time1 = new TimeInterval(30, 50, 40);
+        TimeInterval time2 = new TimeInterval(34,140, 38);
 
-        TimeInterval minute = new TimeInterval();
-        minute.seconds = 60;
-        int secInMinute = minute.getTimeInSec();
+        // С помощью метода класса получаем общее время в секундах для обоих объектов.
+        int time1InSec = time1.getTimeInSec();
+        int time2InSec = time2.getTimeInSec();
+
+        // Сравниваем
+        compareTimeIntervals(time1InSec, time2InSec);
     }
 
+    private static void compareTimeIntervals(int time1, int time2) {
+        if (time1 > time2) {
+            System.out.println(time1 + " > " + time2);
+        } else if (time1 < time2) {
+            System.out.println(time1 + " < " + time2);
+        } else if (time1 == time2) {
+            System.out.println(time1 + " = " + time2);
+        }
+    }
 }
 
 class TimeInterval{
+
     int seconds;
     int minutes;
     int hours;
 
+// Конструктор
+    public TimeInterval(int seconds, int minutes, int hours) {
+        this.seconds = seconds;
+        this.minutes = minutes;
+        this.hours = hours;
+    }
+
+// Метод, который возвращает общее количество секунд
     int getTimeInSec(){
         int totalTimeInSec = seconds + (minutes * 60) + ((hours * 60) * 60);
         System.out.println(totalTimeInSec);
         return totalTimeInSec;
     }
+
+
 }
