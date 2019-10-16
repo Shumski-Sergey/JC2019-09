c0880a069570978ff1d2762113022a28eeb5f8b7
sshumsky <shumski.s.a@gmail.com>
2019-10-16 21:14:10 +0300

    Fix lesson 6 fo timer task

--- src/stitov/Lesson2/Lesson6/Timer/Timer.java
+++ src/stitov/Lesson2/Lesson6/Timer/Timer.java
@@ -2,62 +2,78 @@ package stitov.Lesson2.Lesson6.Timer;
 
 
 public class Timer {
-    public static final int SECOND = 60;
-    public static final int MINUTE = 60;
-    private long seconds;
-    private long minutes;
-    private int hours;
+    private static final int SECOND = 60;
+    private static final int MINUTE = 60;
+    private Long seconds;
+    private Long minutes;
+    private Integer hours;
 
-    public long getSeconds(){return seconds;}
-    public long getMinutes() {return minutes;}
-    public int getHours() {return hours;}
-    public void setSeconds (long sec) {this.seconds=sec;}
-    public void setMinutes (long min) {this.minutes=min;}
-    public void setHours (int h) {this.hours = h;}
-    public void setTimeInSeconds (long sec){
-        if (sec <0) {
+    private Long getSeconds() {
+        return seconds;
+    }
+
+    public Long getMinutes() {
+        return minutes;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setSeconds(long sec) {
+        this.seconds = sec;
+    }
+
+    public void setMinutes(long min) {
+        this.minutes = min;
+    }
+
+    public void setHours(int h) {
+        this.hours = h;
+    }
+
+    private void setTimeInSeconds(long sec) {
+        if (sec < 0) {
             System.out.println("Введено некорректное время");
         } else {
-            long temp;
-            temp = (sec-sec%(SECOND*MINUTE))/(SECOND*MINUTE);
-            this.hours = (int) temp;
-            temp = sec%(SECOND*MINUTE);
-            this.minutes = (temp-temp%SECOND)/SECOND;
-            this.seconds = temp%60;
+            wcSecondsConverter(sec);
         }
     }
-    public void showTime(){
-        System.out.println(this.hours + " hours " + this.minutes + " minutes " + this.seconds+" seconds");
+
+    private void wcSecondsConverter(long sec) {
+        long temp;
+        temp = (sec - sec % (SECOND * MINUTE)) / (SECOND * MINUTE);
+        this.hours = (int) temp;
+        temp = sec % (SECOND * MINUTE);
+        this.minutes = (temp - temp % SECOND) / SECOND;
+        this.seconds = temp % 60;
     }
-    public void showTimeInSeconds(){
+
+    public void showTime() {
+        System.out.println(this.hours + " hours " + this.minutes + " minutes " + this.seconds + " seconds");
+    }
+
+    void showTimeInSeconds() {
         long sec = getTimeInSeconds();
         System.out.println(sec + " seconds");
     }
-    public long getTimeInSeconds () {
-        return (this.hours*SECOND*MINUTE + this.minutes*SECOND + this.seconds);
+
+    long getTimeInSeconds() {
+        return (this.hours * SECOND * MINUTE + this.minutes * SECOND + this.seconds);
     }
-    public int compareTo (Timer t){
-        long thisTime = this.getTimeInSeconds();
-        long otherTime = t.getTimeInSeconds();
-        if (thisTime < otherTime){
-            return -1;
-        } else if (thisTime == otherTime){
-            return 0;
-        } else {
-            return 1;
-        }
+
+    int compareTo(Timer t) {
+        return (this.getSeconds() < t.getSeconds()) ? -1 :
+                ((this.getSeconds().equals(t.getSeconds())) ? 0 : 1);
     }
-    public Timer () {
-        this.seconds = 0;
-        this.minutes = 0;
-        this.hours = 0;
-    }
-    public Timer (long sec, long min, int h) {
+
+    Timer(long sec, long min, int h) {
         this.hours = h;
         this.minutes = min;
         this.seconds = sec;
     }
-    public Timer (long sec) {
+
+    Timer(long sec) {
         this.setTimeInSeconds(sec);
     }
 }
