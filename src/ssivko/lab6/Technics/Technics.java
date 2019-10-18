2af01ae625955cfefe58e8244bdab75f510d5db0
BelIt <Mr.seva@tut.by>
2019-10-18 12:55:22 +0300

    Block six

---
+++ src/ssivko/lab6/Technics/Technics.java
@@ -0,0 +1,18 @@
+package ssivko.lab6.Technics;
+
+public class Technics {
+
+    String color;
+    String materisl;
+    int weight;
+    int cost;
+    boolean onOff;
+
+    void consumption(){
+
+    }
+
+    void waterUse(){
+        
+    }
+}
