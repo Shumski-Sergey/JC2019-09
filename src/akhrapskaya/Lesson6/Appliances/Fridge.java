c3eb6a5c6ee3af7b0975af98f8c71c1987381df6
lapulik <Azxdews1!>
2019-10-18 19:24:07 +0300

    Lesson6

--- src/akhrapskaya/Lesson6/Appliances/Fridge.java
+++ src/akhrapskaya/Lesson6/Appliances/Fridge.java
@@ -1,7 +1,7 @@
 package akhrapskaya.Lesson6.Appliances;
 
 public class Fridge extends LargeSize{
-
+    private final static String name = "Холодильник";
     private Fridge(int power, int enCon) {
         super(power, enCon);
     }
