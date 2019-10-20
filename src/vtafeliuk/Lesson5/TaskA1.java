7911796e28c3c38bb915aa18688c057e7d50bf57
v_tafeliuk <vitali.tafeliuk@gmail.com>
2019-10-20 18:11:19 +0300

    Lesson 4 v.3

--- src/vtafeliuk/Lesson5/TaskA1.java
+++ src/vtafeliuk/Lesson5/TaskA1.java
@@ -7,19 +7,5 @@ import java.util.Scanner;
 public class TaskA1 {
     public static void main(String[] args) {
 
-        String test = "Hello, World!@#$%^&";
-        String testOnlySights = test.replaceAll("(\\w)", "");
-        System.out.println(testOnlySights);
-        String testOnlySights1 = testOnlySights.replaceAll("\\s", "");
-        System.out.println(testOnlySights1);
-        String testWithoutSights = test.replaceAll("\\W", "");
-        System.out.println(testWithoutSights);
-
-        int testbBegin = test.length();
-        int testEnd = testOnlySights1.length();
-
-
-        System.out.println(testbBegin - testEnd);
-
     }
 }
