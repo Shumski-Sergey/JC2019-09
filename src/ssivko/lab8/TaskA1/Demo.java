aa3bc8f3abd7eb018900e08e7b013266366dff20
BelIt <Mr.seva@tut.by>
2019-10-26 09:40:38 +0300

    Block seven

---
+++ src/ssivko/lab8/TaskA1/Demo.java
@@ -0,0 +1,14 @@
+package ssivko.lab8.TaskA1;
+
+public class Demo <T> {
+
+    private T[] item;
+
+    public T[] getItem() {
+        return item;
+    }
+
+    public void setItem(T[] item) {
+        this.item = item;
+    }
+}
