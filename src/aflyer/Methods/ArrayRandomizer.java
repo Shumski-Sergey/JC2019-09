03d7ebeda9475853aabb33091a71af53c62d0bf8
Flyer <Flyer-jr@yandex.com>
2019-10-26 19:34:55 +0300

    Task A1 added using additional class few changes

--- src/aflyer/Methods/ArrayRandomizer.java
+++ src/aflyer/Methods/ArrayRandomizer.java
@@ -3,40 +3,48 @@ package aflyer.Methods;
 import java.util.ArrayList;
 import java.util.Random;
 
-//Заполняем заданный массив случайными числами
-
-public class ArrayRandomizer<I extends Number> {
+public class ArrayRandomizer {
     private ArrayList list;
     private int size;
     private int range;
 
     public ArrayRandomizer() {
-        this.list = new ArrayList<Integer>();
+        this.list = new ArrayList<>();
         this.size = 0;
         this.range = 0;
 
     }
 
-
-    public ArrayRandomizer(ArrayList list, int size, int range){
+    public ArrayRandomizer(ArrayList list, int size, int range) {
         this.list = list;
         this.size = size;
         this.range = range;
     }
 
     /**
-     * @param list given ArrayList for filling
-     * @param size size of ArrayList or number of elements for filling by random numbers
+     * @param list  given ArrayList for filling
+     * @param size  size of ArrayList or number of elements for filling by random numbers
      * @param range range of taking random numbers
      * @return ArrayList with "size" elements filled randomizely from "range"
      */
     public static ArrayList fillArray(ArrayList list, int size, int range) {
         Random m = new Random();
-        for(int i=0; i<size; i++){
+        for (int i = 0; i < size; i++) {
             list.add(i, m.nextInt(range));
         }
         return list;
 
+    }
+
+    /**
+     * OutPuts each element of Arraylist in one line with spaces and takes new line when ends
+     *
+     * @param list given ArrayList for output to console     *
+     */
+    public static void OutputArray(ArrayList<Integer> list) {
+        for (int x : list)
+            System.out.print(x + " ");
+        System.out.print("\n");
 
     }
 
--- src/aflyer/les9_Collections/TaskA1.java
+++ src/aflyer/les9_Collections/TaskA1.java
@@ -12,30 +12,30 @@ public class TaskA1 {
     private static final Integer BADMARK = 30;
 
     public static void main(String[] args) {
-        int kolvo=25;
+        int kolvo = 25;
         int marksrange = 100;
         ArrayList SchoolJournal = new ArrayList();
         SchoolJournal = ArrayRandomizer.fillArray(SchoolJournal, kolvo, marksrange);
-        ouPut(SchoolJournal);
+        ArrayRandomizer.OutputArray(SchoolJournal);
         deletinUnGood(SchoolJournal);
-        ouPut(SchoolJournal);
+        ArrayRandomizer.OutputArray(SchoolJournal);
 
 
     }
 
     private static void ouPut(ArrayList<Integer> list) {
-        for(int i=0; i<list.size(); i++){
-            System.out.print(list.get(i)+" ");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
 
         }
         System.out.print("\n");
     }
 
     private static void deletinUnGood(ArrayList<Integer> list) {
-        for (int i = 0; i<list.size(); ){
+        for (int i = 0; i < list.size(); ) {
             if (list.get(i) <= BADMARK) {
-                list.remove(i);}
-                else i++;
+                list.remove(i);
+            } else i++;
 
         }
 
@@ -43,7 +43,7 @@ public class TaskA1 {
 
     private static void filinIn(ArrayList<Integer> list, int kolvo) {
         Random m = new Random();
-        for (int i=0; i<kolvo; i++){
+        for (int i = 0; i < kolvo; i++) {
             list.add(i, m.nextInt(100));
         }
 
