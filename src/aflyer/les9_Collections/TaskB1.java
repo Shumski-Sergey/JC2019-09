d511dc94b5c6e25ba6669cfde6446149b8bdb810
Flyer <Flyer-jr@yandex.com>
2019-10-30 18:47:39 +0300

    few changes

--- src/aflyer/les9_Collections/TaskB1.java
+++ src/aflyer/les9_Collections/TaskB1.java
@@ -12,22 +12,22 @@ public class TaskB1 {
 
     public static void main(String[] args) throws IOException {
         String filename = "src\\aflyer\\les9_Collections\\Input.txt";
-        String filetext = Files.lines(Paths.get(filename)).reduce("",String::concat);
+        String filetext = Files.lines(Paths.get(filename)).reduce("", String::concat);
         Pattern pattern = Pattern.compile("\\W+");
         String[] text = pattern.split(filetext);
-        HashMap<String,Integer> mapa=new HashMap<>();
-        for(String word: text){
-            int count=1;
-            if (!mapa.containsKey(word)){
+        HashMap<String, Integer> mapa = new HashMap<>();
+        for (String word : text) {
+            int count = 1;
+            if (!mapa.containsKey(word)) {
                 mapa.put(word, count);
             } else {
                 count += mapa.get(word);
-                mapa.replace(word,count);
+                mapa.replace(word, count);
             }
 
         }
-        for (HashMap.Entry<String, Integer> item: mapa.entrySet()) {
-            System.out.println(item.getKey() +" повторяется "+ item.getValue()+" раз");
+        for (HashMap.Entry<String, Integer> item : mapa.entrySet()) {
+            System.out.println(item.getKey() + " повторяется " + item.getValue() + " раз");
 
         }
 
