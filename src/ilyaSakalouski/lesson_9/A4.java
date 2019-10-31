ef1eeabbcf42cbebb32114451bc7eb94c3391394
ilya-sakalouski <ilya6697089@mail.ru>
2019-10-31 18:28:20 +0300

    lesson_9 А1-А4

---
+++ src/ilyaSakalouski/lesson_9/A4.java
@@ -0,0 +1,34 @@
+package ilyaSakalouski.lesson_9;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import static ilyaSakalouski.Different_Utility.UtilityForCollections.*;
+
+
+public class A4
+{
+    public static void main(String[] args) 
+    {
+        final int MILLION = 1000000;     // НЕ ПОМНЮ КАК СДЕЛАТЬ final ПЕРЕМЕННЫЕ КУРСИВОМ(ФИОЛ.ЦВЕТА)
+        final int TEN_THOUSAND = 10000;
+
+        ArrayList<Integer> arrayList;
+        arrayList = GetRandArr(MILLION,TEN_THOUSAND);
+
+        LinkedList<Integer> linkedList;
+        linkedList = GetRandLink(MILLION,TEN_THOUSAND);
+
+        long startArrTime = System.currentTimeMillis();
+        getRandomElements(arrayList,TEN_THOUSAND);
+        long endArrTime = System.currentTimeMillis();
+        long ArrTime = endArrTime - startArrTime;
+        System.out.println("Time for ArrayList: " + ArrTime + "ms");
+
+        long startLinkTime = System.currentTimeMillis();
+        getRandomElements(linkedList,TEN_THOUSAND);
+        long endLinkTime = System.currentTimeMillis();
+        long LinkTime = endLinkTime - startLinkTime;
+        System.out.println("Time for LinkedList: " + LinkTime + "ms");
+    }
+}
