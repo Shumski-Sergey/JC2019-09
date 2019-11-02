c3568d8dab00b4927d562d4a01a71fdf1400f77c
Alexsandr <avsenyuk@hotmail.com>
2019-11-02 15:12:00 +0300

    Merge branch 'master' of C:\Users\Alexsandr\IdeaProjects\JC2019-09 with conflicts.

---
+++ src/asenyuk/Lesson9_Collection_MAP/Lesson9.java
@@ -0,0 +1,25 @@
+package asenyuk.Lesson9_Collection_MAP;
+
+public class Lesson9 {
+
+    public static void main (String [] args){
+
+        String line="123 321 567 213 789";
+String [] words=line.split(" ");
+
+for (String word: words) {
+    //  System.out.println(word);
+    String[] ArrSymbol = word.split("");
+    for (String symbol : ArrSymbol) {
+        System.out.println(symbol);
+        System.out.println("--------");
+        for(String  slovo:  words){
+            slovo.contains(symbol);
+        }
+    }
+
+}
+
+
+    }
+}
--- src/tereshko/NewCollection/Add.java
+++ src/tereshko/NewCollection/Add.java
@@ -1,16 +1,15 @@
 package tereshko.NewCollection;
 
 import java.util.List;
-import java.util.Objects;
 
 public class Add extends Time implements Measure {
-
+    
     @Override
-    public void measureTime(List<Integer> list) {
-        setStart();
-        addList(list);
-        setEnd();
-        System.out.println("Время,которое потратил " + list.getClass().getSimpleName() + " toAdd: " + (getEnd() - getStart()) + "мс");
+        public void measureTime(List<Integer> list) {
+            setStart();
+            addList(list);
+            setEnd();
+            System.out.println("Время, которое потратил "+list.toString()+": "+(getEnd()-getStart()));
+        }
 
-    }
 }
