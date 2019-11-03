e600f25981e48b3797babeac9d9f4a7918f33364
asenyuk <avsenyuk@hotmail.com>
2019-11-03 18:45:34 +0300

    Lesson9_Collection_MAP

--- src/asenyuk/Lesson9_Collection_MAP/Lesson9.java
+++ src/asenyuk/Lesson9_Collection_MAP/Lesson9.java
@@ -1,11 +1,21 @@
 package asenyuk.Lesson9_Collection_MAP;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+/*
+Пользователь вводит через один пробел слова. Найти все группы анаграмм (слов, составленных из одних и тех же букв)
+в этой строке и вывести в консоль их каждую с новой строки, все слова должны идти через пробел в порядке, в котором
+они встречаются в строке. При этом допускается выводить одинаковые слова в одной строке анаграмм. Слова состоящие
+из разного количества одинаковых букв анаграммами не являются
+ */
+
 public class Lesson9 {
     public static void main (String [] args){
         Integer y = 1;
         Integer x=0;
-        String line="123 321 567 213 189";
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine().toLowerCase();
+       // String line="123 321 567 213 189";
 String [] words=line.split(" ");
         HashMap<String,Integer> map0 = new HashMap<>();
   for (String word: words) {
@@ -26,8 +36,7 @@ String [] words=line.split(" ");
         }
     for (Map.Entry<String,Integer> map0Word: map0.entrySet() ) {
         if (map0Word.getValue()==1) {
-          //  System.out.println("Cлово    " +word);
-            System.out.println("имеет следующие анаграммы " + map0Word.getKey() );
+                    System.out.println("имеет следующие анаграммы " + map0Word.getKey() );
                    }
             }
     System.out.println("------");
