8ce44def9cb0451530a4055f0a028e4c5fefe2fc
asenyuk <avsenyuk@hotmail.com>
2019-11-03 18:23:45 +0300

    Lesson9_Collection_MAP

--- src/asenyuk/Lesson9_Collection_MAP/Lesson9.java
+++ src/asenyuk/Lesson9_Collection_MAP/Lesson9.java
@@ -1,25 +1,38 @@
 package asenyuk.Lesson9_Collection_MAP;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Lesson9 {
-
     public static void main (String [] args){
-
-        String line="123 321 567 213 789";
+        Integer y = 1;
+        Integer x=0;
+        String line="123 321 567 213 189";
 String [] words=line.split(" ");
-
-for (String word: words) {
-    //  System.out.println(word);
-    String[] ArrSymbol = word.split("");
+        HashMap<String,Integer> map0 = new HashMap<>();
+  for (String word: words) {
+    map0.clear();
+    for (String word1: words) {
+        map0.put(word1,y);
+    }
+    System.out.println("Cлово    " +word);
+      String[] ArrSymbol = word.split("");
     for (String symbol : ArrSymbol) {
-        System.out.println(symbol);
-        System.out.println("--------");
-        for(String  slovo:  words){
-            slovo.contains(symbol);
+          for (Map.Entry<String,Integer> map0Word: map0.entrySet() ) {
+            if (map0Word.getValue()==1) {
+                if (map0Word.getKey().indexOf(symbol)==-1) {
+                    map0Word.setValue(x);
+                }
+             }
+            }
+        }
+    for (Map.Entry<String,Integer> map0Word: map0.entrySet() ) {
+        if (map0Word.getValue()==1) {
+          //  System.out.println("Cлово    " +word);
+            System.out.println("имеет следующие анаграммы " + map0Word.getKey() );
+                   }
+            }
+    System.out.println("------");
         }
     }
-//
 }
 
 
-    }
-}
