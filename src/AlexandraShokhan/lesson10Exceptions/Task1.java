d5b23ea41234ae8c467720a49ff6e8b213a86b00
Alexandra <alexandra.shokhan@gmail.com>
2019-11-04 19:34:24 +0300

    Lesson 10: Task 1 drafted.

---
+++ src/AlexandraShokhan/lesson10Exceptions/Task1.java
@@ -0,0 +1,23 @@
+package AlexandraShokhan.lesson10Exceptions;
+
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.util.Scanner;
+//
+//public class Task1 {
+//    public static void main(String[] args) throws FileNotFoundException {
+//        readFile("jhbjhb");
+//    }
+//
+//    public static String readFile(String filePath) {
+//        File file = new File(filePath);
+//        try {
+//            Scanner scanner = new Scanner(file);
+//        } catch (FileNotFoundException e) {
+//            System.out.println(e.getMessage());
+//            return "It's text from catch";
+//        } finally {
+//            return "It's text from finally";
+//        }
+//    }
+//}
