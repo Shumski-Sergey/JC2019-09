dfc17e6d80c7496d57038b05bdcfd15f2c23d67b
Woof <7883494@gmail.com>
2019-11-08 21:13:45 +0300

    Task3

--- src/nbogdan/Lesson12Lambda/Task2.java
+++ src/nbogdan/Lesson12Lambda/Task2.java
@@ -5,14 +5,10 @@ package nbogdan.Lesson12Lambda;
 // Записать в другую переменную String строку(через запятую слова),
 // длина которых строго больше 6. В конце запятой не должно быть.
 
-import balexej.lesson8.Array.Array;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Task2 {
     public static void main(String[] args) {
