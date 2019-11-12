80d8d3f9f1f2c7959f32da44de8aa99817386a3a
Burkut60 <burkut60@gmail.com>
2019-11-12 22:29:01 +0300

    1

---
+++ src/balexej/lesson12/Task3.java
@@ -0,0 +1,35 @@
+package balexej.lesson12;
+
+//3. Считать с консоли данные.
+// Вывести во вторую переменную все числа, которые есть в первом файле.
+// Числа выводить через пробел.
+// Пример - 12 text var2 14 8v 1 Результат: 12 14 1
+
+import balexej.Lesson9Colletion.LinkedListArrayList.Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Task3 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку ");
+        String text = sc.nextLine();
+       String[] array = text.split("\\^");
+       Stream<String> stream = Arrays.stream(array);
+        String output = stream.filter(value -> {
+            Pattern pattern = Pattern.compile("\\d");
+            Matcher matcher = pattern.matcher(value);
+            if (matcher.matches()) {
+                return true;
+            } else
+                return false;
+        }).collect(Collectors.joining(" "));
+        System.out.println(output);
+    }
+    }
+
