05d838ecc95aca6675c78fe801b34a513836a325
asenyuk <avsenyuk@hotmail.com>
2019-11-12 23:56:51 +0300

    Lesson12

--- src/asenyuk/Lesson12_Date_Time_API/Task1.java
+++ src/asenyuk/Lesson12_Date_Time_API/Task1.java
@@ -7,7 +7,6 @@ import java.util.Date;
 public class Task1 {
     public static void main(String[] args) {
 
-
         Date date = new Date();
         SimpleDateFormat simpleDateformat = new SimpleDateFormat("E");
          simpleDateformat = new SimpleDateFormat("EEEE");
