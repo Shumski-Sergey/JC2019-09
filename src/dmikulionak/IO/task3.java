c0401a54b12f8e7f463967ddf6ceb58bef3b71de
mikulionk <dimaborovka@gmail.com>
2019-11-20 13:45:52 +0300

    IO

--- src/dmikulionak/IO/task3.java
+++ src/dmikulionak/IO/task3.java
@@ -2,5 +2,35 @@ package dmikulionak.IO;
 //3.Организовать построчное считывание данных, введенных с консоли,
 // и их запись в файл. Признаком окончания записи (конец работы программы) является строка со словом «#ESC».
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
 public class task3 {
+    private static String path = "D:\\testTEXT\\text.txt";
+
+    public static void main(String[] args) throws IOException {
+        File text = new File(path);
+        boolean newFile = text.createNewFile();
+        System.out.println(" новый файл создан ? : " + text.exists());
+
+        Scanner in = new Scanner(new InputStreamReader(System.in));
+        String input;
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(text)));
+        while (true) {
+            input = in.nextLine();
+            if (input.equals("#ESC")) {
+                writer.flush();
+                break;
+            }
+            writer.write(input + System.lineSeparator());
+        }
+        writer.close();
+        in.close();
+    }
 }
+
