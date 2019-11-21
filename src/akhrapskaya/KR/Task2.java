c32784022d7757816aa3785546561be0508d9c7d
lapulik <Azxdews1!>
2019-11-21 23:03:11 +0300

    KR

--- src/akhrapskaya/KR/Task2.java
+++ src/akhrapskaya/KR/Task2.java
@@ -1,21 +1,22 @@
 package akhrapskaya.KR;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Task2 {
-    public static void  main(String[] args){
-        String s = getStringFromConsole();
-        if (s.matches("[\\d]+") && Integer.parseInt(s) > 0 && Integer.parseInt(s) < 13){
-            System.out.println( new Month(Integer.parseInt(s)).getMonth());
-        }
-        else
-            System.out.println("Некорректно введены данные!");
-
-    }
-
-    private static String getStringFromConsole() {
+    public static void  main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число в диапазоне [1-12]");
-        return sc.nextLine();
+        try {
+
+            System.out.println(new Month(sc.nextInt()).getMonth());
+        } catch (InputMismatchException e) {
+            System.out.println("Некорректно введены данные!");
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("Введено недопустимое число!");
+        }
     }
+
 }
+
